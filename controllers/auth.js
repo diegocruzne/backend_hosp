28b168e725209ed752cf58131d93b47efc7a0f50
diego <diego_fcb_96@hotmail.com>
2023-06-03 23:20:06 -0500

    3 junio

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
 const { googleVerify } = require("../helpers/google-verify");
+const { getMenuFrontEnd } = require("../helpers/menu-frontend");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -30,6 +31,7 @@ const login = async (req, res = response) => {
     res.json({
       ok: true,
       token,
+      menu: getMenuFrontEnd(usuarioDB.role),
     });
   } catch (error) {
     console.log(error);
@@ -73,6 +75,7 @@ const googleSignIn = async (req, res = response) => {
     res.json({
       ok: true,
       token,
+      menu: getMenuFrontEnd(usuario.role),
     });
   } catch (error) {
     res.status(401).json({
@@ -95,6 +98,7 @@ const renewToken = async (req, res = response) => {
     ok: true,
     token,
     usuario,
+    menu: getMenuFrontEnd(usuario.role),
   });
 };
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -9,7 +9,11 @@ const {
   actualizarUsuario,
   borrarUsuario,
 } = require("../controllers/usuario");
-const { validarJWT } = require("../middlewares/validar-jwt");
+const {
+  validarJWT,
+  validarADMIN_ROLE,
+  validarADMIN_ROLE_o_MismoUsuario,
+} = require('../middlewares/validar-jwt');
 
 const router = Router();
 
@@ -27,17 +31,18 @@ router.post(
 );
 
 router.put(
-  "/:id",
+  '/:id',
   [
     validarJWT,
-    check("nombre", "El nombre es obligatorio").not().isEmpty(),
-    check("email", "El email es obligatorio").isEmail(),
-    check("role", "El role es obligatorio").not().isEmpty(),
+    validarADMIN_ROLE_o_MismoUsuario,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('role', 'El role es obligatorio').not().isEmpty(),
     validarCampos,
   ],
   actualizarUsuario
 );
 
-router.delete("/:id", validarJWT, borrarUsuario);
+router.delete('/:id', [validarJWT, validarADMIN_ROLE], borrarUsuario);
 
 module.exports = router;
